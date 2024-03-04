9cf28577eede20898278a44632e064e93fec731e
hozrot <hozrot.ali64@gmail.com>
2024-03-04 15:51:27 +0600

    API testing and on precess..2nd look problem with map

--- screens/Calender.js
+++ screens/Calender.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,FlatList, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, ScroolView } from 'react-native'
 
 import React, { useState, useEffect } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -6,11 +6,13 @@ import TextCard from '../component/TextCard';
 
 import axios from 'axios';
 
-const API_URL = 'https://api.aladhan.com/v1/asmaAlHusna';
+// const API_URL = 'https://jsonplaceholder.typicode.com/posts/1';
+const API_URL = 'https://api.aladhan.com/v1/asmaAlHusna/4';
+
 export default function Calender({ navigation }) {
   const [data, setData] = useState([]);
+  const [mydata, setMydata] = useState([]);
 
-  const  myJsonString = JSON.stringify(data);
   useEffect(() => {
     fetchData();
   }, []);
@@ -19,32 +21,35 @@ export default function Calender({ navigation }) {
     try {
       const response = await axios.get(API_URL);
       setData(response.data);
+
+      // const extractMydata = mydata.map((data)=>data[1]);
+      // setMyData(response.extractMydata);
       console.log(data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
   return (
+
+
     <View style={styles.container}>
-      {/* <Text style={styles.title}>Making API Requests</Text>
-      <View style={styles.item}>
-            <Text>test {data.number.tostring()}</Text>
-            <Text>test2 </Text>
-          </View> */}
- <Text style={styles.title}>Making API Requests</Text>
- <Text>test {myJsonString.name}</Text>
- <Text>test {data.code}</Text>
- <Text>test {data.number}</Text>
+      
+      <Text style={styles.title}>Making API Requests</Text>
+    
+     
+
+      
+   
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:70,
+    marginTop: 70,
     flex: 1,
-    justifyContent:'center',
-    alignItems:'center',
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
     backgroundColor: '#fff',
   },
